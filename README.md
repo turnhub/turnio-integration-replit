649bb3e087cbb3c8254beb61552d95ce1165cf96
Simon de Haan <simon@turn.io>
2021-05-05 10:20:51 +0000

    demo

---
+++ README.md
@@ -0,0 +1,26 @@
+# turnio-integration-replit
+
+An example Turn.io UI integration on Replit. This will augment the Turn UI with custom profile information, a suggested reply, and a custom action menu.
+
+*This Replit assumes the following*:
+
+1. You have gone through the [webhooks example](https://github.com/turnhub/turnio-webhooks-replit) and are comfortable with how to get an authentication for Turn and set it up as a secret in Repl.it
+2. You have gone through the [DuckDuckGo suggested reply example](https://github.com/turnhub/turnio-duckduckgo-replit) and are comfortable with how to set up a `Custom Integration` in Turn.
+
+# How to run this Repl.it
+
+[![Run on Repl.it](https://repl.it/badge/github/turnhub/turnio-integration-replit)](https://repl.it/github/turnhub/turnio-integration-replit)
+
+1. Click the `Run on Repl.it` button above and install this example into your Repl.it workspace.
+2. Get a Turn token and add it as a secret called `TOKEN` in Repl.it
+3. Sign-up for a free [Giphy developer API account](https://developers.giphy.com) and obtain an API token, add that as a secret called `GIPHY_API_TOKEN` in Repl.it
+4. Set up the custom integration using the URL generated for your workspace by Repl.it
+5. Send a message to your number and interact with the UI!
+
+Here's a screenshot showing the custom profile addition, the suggested reply, and the roll a dice action menu:
+
+![screenshot1](./screenshot-integration-01.png)
+
+Here's a screenshot showing the Giphy menu when a message contains the word `gif`:
+
+![screenshot2](./screenshot-integration-02.png)
---
+++ main.py
@@ -0,0 +1,259 @@
+from flask import Flask, request
+import os
+import phonenumbers
+import requests
+import random
+
+app = Flask('app')
+
+# Repl provides these environment variables, we use it
+# to construct the URL that this repl will be accessible on.
+repl_owner = os.environ.get("REPL_OWNER")
+repl_slug = os.environ.get("REPL_SLUG")
+repl_url = "https://%s.%s.repl.co" % (repl_slug, repl_owner)
+
+TOKEN = os.environ.get("TOKEN")
+GIPHY_API_KEY = os.environ.get("GIPHY_API_KEY")
+
+@app.route('/')
+def hello_world():
+  return 'The Turn UI integration API endpoint is at %s/integration' % (repl_url,)
+
+@app.route('/integration', methods=['POST'])
+def integration():
+  json = request.json
+  # If the JSON payload has a handshake, return the capabilities
+  # of this integration. This way we inform Turn of what this 
+  # integration is capable of.
+  if json.get("handshake", False):
+    return {
+      "version": "1.0.0-alpha",
+      "capabilities": {
+        "actions": True,
+        "suggested_responses": True,
+        "context_objects": [
+          {
+            "title": "My Custom Profile",
+            "code": "my-custom-profile",
+            "type": "table"
+          }
+        ]
+      }
+    }
+
+  # As part of the payload we receive the recent messages
+  # sent and received in the chat on Turn. 
+  # Here we select out the messages that were received (direction 
+  # equals inbound) and which are of type text.
+  text_messages = [
+    message for message in json["messages"]
+      if message["type"] == "text" and message["_vnd"]["v1"]["direction"] == "inbound"
+    ]
+  ordered_by_most_recent = sorted(text_messages, key=lambda message: message["timestamp"], reverse=True)
+  # We select the most recent message
+  message = ordered_by_most_recent[0]
+  
+  response = {
+    "version": "1.0.0-alpha",
+    "suggested_responses": get_suggested_responses(message),
+    "actions": get_actions(message),
+    "context_objects": get_context_objects(message),
+  }
+  return response
+
+def get_suggested_responses(message):
+  """
+  If a message starts with the word "join", which in the case of
+  Turn likely means that someone's joined a sandbox for the first
+  time, then we'll send a welcome message as a suggested response.
+  """
+  text = get_text(message)
+  profile_name = get_profile_name(message)
+
+  if text.startswith("join"):
+    return [{
+      "type": "TEXT",
+      "title": "Welcome message",
+      "confidence": 1.0,
+      "body": """Hi _%s_ 👋, and *welcome* to this WhatsApp service!
+      
+This example extends the Turn UI in 3 ways.
+
+1️⃣ It adds this text as a suggested reply in the UI for any message starting with the word _join_
+
+2️⃣ It adds a custom UI extension called "My Custom Profile" in the sidebar on the left which displays country information based on your phone number's country code prefix.
+
+3️⃣ It adds extra action menu items in your chat window. If the last message received has the word "gif" in it it will add a menu to search for the text on Giphy.com, otherwise it will only display a menu that allows you to roll a dice and send a reply.
+
+Enjoy! 🎉 """ % (profile_name,)
+    }]
+  else:
+    return []
+
+def get_actions(message):
+  """
+  This will return a set of actions that will be added to the 
+  message action menu in the Turn UI for this conversation.
+  """
+  text = get_text(message)
+
+  actions = {
+    "roll_the_dice": {
+      "description": "Roll 🎲",
+      "url": "%s/action/roll_the_dice" % (repl_url,),
+      "payload": {
+        "some-custom": "payload"
+      },
+      "options": {
+        "roll_once": "Roll one time",
+        "roll_twice": "Roll two times",
+      }
+    }
+  }
+  if "gif" in text:
+    actions.update({
+      "giphy": {
+        "description": "Auto Giphy ⚡️",
+        "url": "%s/action/giphy" % (repl_url,),
+        "payload": {
+          "text": text
+        }
+      }
+    })
+  return actions
+
+@app.route("/action/roll_the_dice", methods=["POST"])
+def roll_the_dice():
+  """
+  This handles the event when some clicks on the action menu
+  to either roll the die once or twice.
+
+  We send a reply via the Turn API with the result.
+  """
+  message = request.json["message"]
+  option = request.json["option"]
+  profile_name = get_profile_name(message)
+  wa_id = message["from"]
+  if option == 'roll_once':
+    first_roll = random.randint(1, 6)
+    response = send_message(
+      wa_id, "Hi %s, you rolled %s!" % (profile_name, first_roll,))
+  elif option == 'roll_twice':
+    first_roll = random.randint(1, 6)
+    second_roll = random.randint(1, 6)
+    response = send_message(
+      wa_id, "Hi %s, you rolled %s and %s!" % (
+        profile_name, first_roll, second_roll))
+  return ''
+
+@app.route("/action/giphy", methods=["POST"])
+def giphy():
+  """
+  Search for a gif using the giphy API as per their docs at 
+  https://developers.giphy.com/docs/api/endpoint/#search
+  """
+  message = request.json["message"]
+  wa_id = message["from"]
+  text = get_text(message).lstrip("gif")
+  r = requests.get("https://api.giphy.com/v1/gifs/search", {
+    "api_key": GIPHY_API_KEY,
+    "q": text,
+    "rating": "g",
+    "lang": "en"
+  })
+  response_json = r.json()
+  data = response_json["data"]
+  if data: 
+    gif = data[0]
+    mp4_url = gif["images"]["fixed_height"]["mp4"]
+    send_video(wa_id, mp4_url)
+  else:
+    send_message(wa_id, "Sorry couldn't find a gif for %s" % (text,))
+  return ''
+
+def send_message(wa_id, body):
+  """
+  Send a text message to a wa_id
+  """
+  return requests.post(
+    url="https://whatsapp.turn.io/v1/messages",
+    headers={
+        "Authorization": f"Bearer {TOKEN}",
+        "Content-Type": "application/json",
+    },
+    json={
+        "to": wa_id,
+        "text": {
+            "body": body
+        },
+    })
+
+def send_video(wa_id, mp4_url):
+  """
+  Send a video message to a wa_id
+  """
+  return requests.post(
+    url="https://whatsapp.turn.io/v1/messages",
+    headers={
+      "Authorization": f"Bearer {TOKEN}",
+      "Content-Type": "application/json",
+    },
+    json={
+      "to": wa_id,
+      "type": "video",
+      "video": {
+        "link": mp4_url
+      }
+    }
+  )
+
+def get_context_objects(message):
+  """
+  This will generate the response for the custom profile
+  block in the left column in the Turn UI.
+  """
+
+  wa_id = message["from"]
+  phone_number = phonenumbers.parse("+%s" % (wa_id,), None)
+  response = requests.get('https://restcountries.eu/rest/v2/callingcode/%s' % (phone_number.country_code,))
+  data = response.json()
+  if data:
+    country = data[0]
+    return {
+      "my-custom-profile": {
+        "Region": country["region"],
+        "Country": country["name"],
+        "Capital": country["capital"]
+      }
+    }
+  else:
+    return {
+      "my-custom-profile": {
+      "Region": "Unknown",
+      "Country": "Unknown",
+      "Capital": "Unknown"
+    }}
+
+
+
+### Utility methods below
+
+def get_text(message):
+  """
+  This returns the text of the message received.
+  It assumes the message is of type "text".
+  """
+  return message["text"]["body"]
+
+def get_profile_name(message):
+  """
+  This returns the profile name of the WhatsApp contact.
+  This payload is received directly from the WhatsApp network
+  and reflects the profile name currently active on the phone's
+  client -- this can change between messages if the name on the
+  phone's client changes.
+  """
+  return message["_vnd"]["v1"]["author"]["name"]
+
+
+app.run(host='0.0.0.0', port=8080)
